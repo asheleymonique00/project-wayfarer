61d72fe1c4e07e03b8681df50e814c6b359d91ba
Asheley Brown <ABrown119@humana.com>
2020-07-08 15:04:38 -0400

    Login, Logout, Signup done

--- project-3/src/App.js
+++ project-3/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import SignUp from './Components/SignUp'
+import { Route } from 'react-router-dom';
+
+import SignUp from './Components/SignUp';
+import Login from './Components/Login';
+
+import { signUpUser, loginUser, verifyUser } from './Service/api_helper';
 
 class App extends Component {
   constructor(props) {
@@ -14,17 +19,53 @@ class App extends Component {
   handleSignUp = async (e, user) => {
     e.preventDefault();    
     const loadedUser = await signUpUser(user)
+    console.log(loadedUser);
     this.setState({
       currentUser: loadedUser
+      
     })
     
   }
+
+  async componentDidMount() {
+    const currentUser = await verifyUser();
+    if (currentUser) {
+      this.setState({
+        currentUser: currentUser
+      })
+    }
+  }
+
+  handleLogin = async (e, user) => {
+    e.preventDefault();
+    const loadedUser = await loginUser(user);
+
+    this.setState({
+      currentUser: loadedUser
+    })
+  }
+
+  handleLogout = () => {
+    this.setState({
+        currentUser: null
+    })
+    localStorage.removeItem('authToken');
+
+}
+
 
   render() {
   return (
     <div className="App">
       <header className="App-header">
-      <SignUp handleSubmit={this.handleSignUp} />
+        <h2>Wayfarer</h2>
+        {this.state.currentUser ? <button onClick={this.handleLogout}>Logout</button> : (
+        <div>
+          <SignUp handleSubmit={this.handleSignUp} />
+          <Login handleSubmit={this.handleLogin}/>
+        </div>
+      )}
+
       </header>
     </div>
   );
--- project-3/src/Service/api_helper.js
+++ project-3/src/Service/api_helper.js
@@ -3,7 +3,29 @@ const api = axios.create({ baseURL: "http://localhost:3001" })
 
 export const signUpUser = async (signUpData) => {
     const userData = await api.post('/auth/signup', signUpData);
+    console.log(userData);
     localStorage.setItem('authToken', userData.data.token);
     api.defaults.headers.common.authorization = `Bearer ${userData.data.token}`;
-    return userData.data.user;
+    return userData.config.data;
   }
+
+  export const loginUser = async (loginData) => {
+    const userData = await api.post('/auth/login', loginData);
+    console.log(userData);
+    localStorage.setItem('authToken', userData.data.token);
+    api.defaults.headers.common.authorization = `Bearer ${userData.data.token}`;
+    return userData.config.data;
+  }
+
+ 
+export const verifyUser = async () => {    
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      api.defaults.headers.common.authorization = `Bearer ${token}`;
+      const userData = await api.get('/auth/verify');
+      return userData.config.data;
+    } else {
+      return false;
+    }
+  }
+  
