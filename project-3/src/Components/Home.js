3940e9fc36e61791f083b8ff191d5d62fc965e45
Mario Lozano <mario_x_lozano@glic.com>
2020-07-14 13:56:10 -0400

    Home page is now component

--- project-3/src/Components/Home.js
+++ project-3/src/Components/Home.js
@@ -8,34 +8,35 @@ class Home extends Component {
     super(props);
     this.state = {
       topic1: {
-      title: "Review",
-      body: "tgysgsfdgsdgsdgsdgsd"
+      title: "Get to San Francisco Food Tours",
+      body: "Sidewalk Food Tours of San Francisco is a culinary experience company offering delicious, informative, and fun walking food tours in three distinctly different neighborhoods."
       },
       topic2: {
-      title: "Other review",
-      body: "gfdgsdgsdgsdgsd"
+      title: "The Best of Brooklyn",
+      body: "Brooklyn is big! If it was not part of New York City, it would still be the 4th largest city in the USA; it has 43 distinct neighborhoods full of diversity with people from almost every country on Earth. With that comes all of the amazing traditions, history, and mouth-watering food they’ve brought with them. To put it simply, no trip to New York is complete without a stop across the bridge to experience and eat your way through Brooklyn."
       },
       topic3: {
-      title: "Comment",
-      body: "Great app"
-      },
+      title: "Sydney Food Tours: Discover what the locals know and where the foodies go!",
+      body: "Each tour is a back-stage pass to Sydney's authentic and modern multicultural cuisines, all enhanced by Australia's freshest local ingredients. The tours support diverse local communities through training programs and employment opportunities."
+      }
     }
   }
   
     render () {
     return (
-      <div>
+      <div className="home">
         <h1>Wayfarer is ...</h1>
         <div className="reviewBox">
-          <span className="box">
+        
+          <span className="homeBox">
           <h1>{this.state.topic1.title}</h1>
           <p>{this.state.topic1.body}</p>
           </span>
-          <span className="box">
+          <span className="homeBox">
           <h1>{this.state.topic2.title}</h1>
           <p>{this.state.topic2.body}</p>
           </span>
-          <span className="box">
+          <span className="homeBox">
           <h1>{this.state.topic3.title}</h1>
           <p>{this.state.topic3.body}</p>
           </span>
